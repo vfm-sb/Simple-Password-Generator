26e8d7084a1227e81c064917c353ee64c33b24ac
VFM | SB <vfm_sb@proton.me>
2022-11-28 01:28:09 +0200

    Changed License to MIT and Provided LICENSE File

--- password_generator.py
+++ password_generator.py
@@ -5,7 +5,7 @@ Simple Password Generator
 __author__ = "VFM | SB"
 __email__ = "vfm_sb@proton.me"
 __copyright__ = "Copyleft 2022"
-__license__ = "GNU GPLv3"
+__license__ = "MIT"
 __version__ = "0.1.1"
 __maintainer__ = "VFM | SB"
 __status__ = "Development"
